b424b76061817167aef073ed3f6801d8fe8ac1b4
Pixelsuft <yuskivl07@mail.ru>
2025-06-03 19:35:20 +0700

    refactor

--- src/audio_sdl2_mixer.cpp
+++ src/audio_sdl2_mixer.cpp
@@ -1,16 +1,20 @@
-#include <audio_sdl2_mixer.hpp>
+#include <audio_base.hpp>
 #include <new.hpp>
 #include <log.hpp>
 
-using audio::AudioSDL2Mixer;
+namespace audio {
+    class AudioSDL2Mixer : public AudioBase {
+        public:
+        AudioSDL2Mixer(bool use_mixer_x) {
+            TF_INFO(<< "TODO: SDL2_mixer support!");
+        }
 
-AudioSDL2Mixer::AudioSDL2Mixer(bool use_mixer_x) {
-    TF_INFO(<< "TODO: SDL2_mixer support!");
-}
+        ~AudioSDL2Mixer() {
 
-AudioSDL2Mixer::~AudioSDL2Mixer() {
+        }
+    };
 }
 
 audio::AudioBase* audio::create_sdl2_mixer(bool use_mixer_x) {
-    return (audio::AudioBase*)tf::nw<AudioSDL2Mixer>(use_mixer_x);
+    return (audio::AudioBase*)tf::nw<audio::AudioSDL2Mixer>(use_mixer_x);
 }
