f8d892c7a8ade04c69e924302f24a0d8888be4e4
Pixelsuft <yuskivl07@mail.ru>
2025-06-16 14:08:49 +0700

    wip

--- include/res.hpp
+++ include/res.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include <SDL3/SDL_iostream.h>
 
 namespace res {
+    SDL_IOStream* get_asset_io(const char* fn);
     void* read_asset_data(const char* fn, size_t& size_buf);
     void free_asset_data(void* data);
 }
--- src/res.cpp
+++ src/res.cpp
@@ -4,12 +4,24 @@
 #include <SDL3/SDL.h>
 // TODO: optimize for release mode
 
+SDL_IOStream* res::get_asset_io(const char* fn) {
+    const char* path = nullptr;
+    if (!SDL_strcmp(fn, "icon.png"))
+        path = "assets/icon.png";
+    else
+        TF_ERROR(<< "Unknown asset: " << fn);
+    SDL_IOStream* ret = SDL_IOFromFile(path, "rb");
+    if (!ret) {
+        TF_ERROR(<< "Failed to open io from asset file (" << SDL_GetError() << ")");
+        return nullptr;
+    }
+    return ret;
+}
+
 void* res::read_asset_data(const char* fn, size_t& size_buf) {
     const char* path = nullptr;
     if (!SDL_strcmp(fn, "Roboto-Regular.ttf"))
         path = "assets/Roboto-Regular.ttf";
-    else if (!SDL_strcmp(fn, "icon.png"))
-        path = "assets/icon.png";
     else
         TF_ERROR(<< "Unknown asset: " << fn);
     void* ret = SDL_LoadFile(path, &size_buf);
